9ead0364745292daf9ee940f62e7a02ec37bd340
Jos <feenstrajos@gmail.com>
2021-01-09 23:27:35 +0100

    make build

---
+++ docs/readme.md
@@ -0,0 +1,1 @@
+github recognises the docs folder, and auto publishes it.
--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,13 @@ const path = require('path');
 // this is how to access ...
 module.exports = (env, args) => {
     const isProductionMode = (args.mode === 'production');
-
+    const buildpath = isProductionMode ?  'docs' : 'build';
 
     return {
         entry: './index.js',
         output: {
             // using hashes make sure that old cashes are succcesfully taken care off
-            path: path.resolve(__dirname, 'build'),
+            path: path.resolve(__dirname, buildpath),
             filename: isProductionMode ? '[name].[contenthash].js' : '[name].[hash].js',
         },
         plugins: [
